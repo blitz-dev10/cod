7c29834d68b2cfe945cb941649b57b07cf4dc536
blitz-dev10 <dhruv.dornal.dev@gmail.com>
2025-04-05 14:34:09 +0530

    Update migration.sql

--- prisma/migrations/20250401092042_dhruv/migration.sql
+++ prisma/migrations/20250401092042_dhruv/migration.sql
@@ -4,9 +4,28 @@ CREATE TABLE "User" (
     "username" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "points" INTEGER DEFAULT 0,
+    "premium" BOOLEAN DEFAULT FALSE,
+    "premium_expiry" TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateTable
+CREATE TABLE "progress" (
+  "id" SERIAL NOT NULL,
+  "user_id" INT NOT NULL,
+  "lesson_id" INT NOT NULL,
+  "completed" BOOLEAN NOT NULL,
+  "score" INT NOT NULL,
+  "date_completed" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  
+  CONSTRAINT "progress_pkey" PRIMARY KEY ("id")
+);
+
+-- Add initial unique constraint
+ALTER TABLE "progress" ADD CONSTRAINT "progress_user_lesson_unique" 
+  UNIQUE (user_id, lesson_id);
